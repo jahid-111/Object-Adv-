279c512f4cb206c297beb18722ca067ac8af46d5
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-08 22:55:31 +0300

    first commit

---
+++ obj.js
@@ -0,0 +1,22 @@
+// console.log("obj.js")
+
+
+// LITERAL OBJECT 
+const obj = {
+        Prefix : "Mohd",
+        First : 'Jahidul',
+        Activity : function (){
+              return  this.hobby
+        },
+        Last : 'Islam',
+        Age : 25,
+        Gender : "Male",
+}
+obj.hobby = ["Web Developer", "Full-Stack Developer"]
+
+console.log(obj.Activity())
+
+
+
+// OBJECT CONSTRACTOR 
+
