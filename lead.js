aa0430f2921965c0c7705a3b0b0ce15dcf150a40
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-09 03:50:45 +0300

    LoopObject

--- lead.js
+++ lead.js
@@ -14,11 +14,27 @@ const keys  =Object.keys(product)
 // console.log(keys)
 
 const valus = Object.values(product);
-console.log(valus)
+// console.log(valus)
 
 const objVal = Object.entries(product) // Print => Multi Dimensional Array  'With Prop and Value'
-Object.seal(product) // You cant Delete any prop valu of Obj  ##JS Always Run Line by Line
+// Object.seal(product) // You cant Delete any prop valu of Obj  ##JS Always Run Line by Line
 // product.price = 1400,   //seal can Change the Obj value
-Object.freeze(product) 
-delete product.brand
-console.log(product)  
+// Object.freeze(product) 
+// delete product.brand
+// console.log(product)  
+
+
+
+for( const key in product) {
+    // console.log(product[key])
+}
+
+
+for(const key in product){
+    // console.log(product[key])
+}
+
+
+for(const [key,valu] of Object.entries(product)){
+    console.log(key,valu)
+}
