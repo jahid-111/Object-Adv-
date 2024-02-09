8dcab5bd57f9e748721e95e2b526c6845577a2a0
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-09 03:24:20 +0300

    LeadingObj

---
+++ lead.js
@@ -0,0 +1,24 @@
+// console.log('lead.js')
+
+const product =  {
+
+        brand  : "Apple",
+        name : "iPhone 15 pro Max++ ",
+        storage : "1TB",
+        ram : "8BG",
+        color : "white",
+        price : 1399
+}
+
+const keys  =Object.keys(product)
+// console.log(keys)
+
+const valus = Object.values(product);
+console.log(valus)
+
+const objVal = Object.entries(product) // Print => Multi Dimensional Array  'With Prop and Value'
+Object.seal(product) // You cant Delete any prop valu of Obj  ##JS Always Run Line by Line
+// product.price = 1400,   //seal can Change the Obj value
+Object.freeze(product) 
+delete product.brand
+console.log(product)  
